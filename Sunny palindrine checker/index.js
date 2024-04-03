aab6a7563472d7c664ad89772bfa7e4888e6d861
Sxnny-s <115429120+Sxnny-s@users.noreply.github.com>
2024-04-03 10:20:48 -0400

    i addded a color changing button

--- Sunny palindrine checker/index.js
+++ Sunny palindrine checker/index.js
@@ -8,12 +8,29 @@ function check() {
     const value = input.value;
     const reverse = reverseString(value)
 
-    if (value === reverse){
+    if (value === reverse && value !== ''){
         alert("PALINDROME COMFIRMED")
-    }else{
+    }else if (value === ''){
+        alert("please type a value")
+    }
+    else{
         alert("PALINDROME DENIED")
     }
 
     input.value = ""
+}
+// end if palindrome checker
+// adding color switcher to app
+
+const button = document.getElementById('change');
+
+function colorChange(){
+
+    const red = Math.random() * 255;
+    const blue = Math.random() * 255;
+    const green = Math.random() * 255;
+
+
+    button.style.backgroundColor = `rgb(${red}, ${blue}, ${green})`
 }
 
